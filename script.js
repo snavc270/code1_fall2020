b1563dcb92d1101241c819d46aa887af4d885c65
Courtney Snavely <courtney@ovee.me>
2020-09-23 14:26:14 -0400

    added week4 notes

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ class Header extends HTMLElement {
                 <a href="week1.html"> Week 1</a>
                 <a href="week2.html"> Week 2</a>
                 <a href="week3.html"> Week 3</a>
+                <a href="week4.html"> Week 4</a>
               </div>
             </div>
           </li> 
